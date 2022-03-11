a295df3c647f625537686046e64dd38d201c169f
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-03-11 09:06:15 +0800

    打卡时间修改

--- main.go
+++ main.go
@@ -35,8 +35,8 @@ func main() {
 	r.DELETE("/report", api.EndReport)
 
 	c := cron.New()
-	_ = c.AddFunc("* */5 4-23 * * *", Service.ReportAll)
-	_ = c.AddFunc("* * 0-4/5 * * *", Service.ReportInOrder)
+	_ = c.AddFunc("* */10 4-23 * * *", Service.ReportAll)
+	_ = c.AddFunc("* * 0-4/4 * * *", Service.ReportInOrder)
 
 	c.Start()
 
