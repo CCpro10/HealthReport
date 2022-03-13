617b9185489ccf794201968fb3db84afa9f7ee72
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-03-13 18:16:45 +0800

    可以看有多少人用了自动健康打卡

---
+++ count.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"main/Model"
+	"net/http"
+	"strconv"
+)
+
+var C int64
+
+func main() {
+
+	Model.InitMySQL()
+
+	http.HandleFunc("/", CountHandler)
+	_ = http.ListenAndServe("127.0.0.1:8889", nil)
+}
+
+// handler函数
+func CountHandler(w http.ResponseWriter, r *http.Request) {
+	Model.DB.Model(&Model.Student{}).Count(&C)
+	w.Write([]byte("总共有" + strconv.FormatInt(C, 10) + "人使用了自动健康打卡"))
+}
